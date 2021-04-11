1a86fd387dda23e43e536f364a77b796d3139ff9
Vadim-Vatrya <VatryaVV@gmail.com>
2021-04-11 18:58:41 +0300

    create App

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
-import { useEffect } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { getLoading } from './redux/contacts/contact-selectors';
-import {fetchContacts} from './redux/contacts/contact-operations';
+// import { useEffect } from 'react';
+// import { useSelector, useDispatch } from 'react-redux';
+
+import { Switch, Route, Redirect } from 'react-router-dom';
+// import { getLoading } from './redux/contacts/contact-selectors';
+// import {fetchContacts} from './redux/contacts/contact-operations';
 
 import Container from './components/Container';
 import ContactForm from './components/ContactForm';
@@ -9,41 +11,65 @@ import Filter from './components/Filter';
 import ContactList from './components/ContactList';
 import Loader from './components/Loader';
 
+
+
 const App = () => {
-    // const contacts = useSelector(getContacts);
-    const loading = useSelector(getLoading);
-    const dispatch = useDispatch();
-
-    useEffect(() => {
-        dispatch(fetchContacts());
-    }, [dispatch]);
-
 
     return (
-    <Container>
-        <h1>Phonebook</h1>
-        <ContactForm />
+        <Container>
+            <AppBar />
 
-        <h2>Contacts</h2>
-        {/* {loading && <Loader />} */}
-        <Loader
-          type="Puff"
-          color="#000"
-          height={70}
-          width={70}
-          visible={loading}
-        />
-        {/* {contacts.length > 0 
-        ? ( */}
-        <>
-         <Filter />
-         <ContactList />
-        </>
-        {/* )
-        : (<p>No contacts</p>)} */}
-       
-    </Container>
+            <Switch>
+                <Route exact path="/" component={HomeView} />
+                <Route path="/contacts" component={ContactsView} />
+                <Route path="/register" component={RegisterView} />
+                <Route path="/login" component={LoginView} />
+                <Redirect to="/" />
+            </Switch>
+        </Container>
+        
+
     )
 };
 
 export default App;
+
+
+
+
+// const App = () => {
+//     // const contacts = useSelector(getContacts);
+//     const loading = useSelector(getLoading);
+//     const dispatch = useDispatch();
+
+//     useEffect(() => {
+//         dispatch(fetchContacts());
+//     }, [dispatch]);
+
+
+//     return (
+//     <Container>
+//         <h1>Phonebook</h1>
+//         <ContactForm />
+
+//         <h2>Contacts</h2>
+//         {/* {loading && <Loader />} */}
+//         <Loader
+//           type="Puff"
+//           color="#000"
+//           height={70}
+//           width={70}
+//           visible={loading}
+//         />
+//         {/* {contacts.length > 0 
+//         ? ( */}
+//         <>
+//          <Filter />
+//          <ContactList />
+//         </>
+//         {/* )
+//         : (<p>No contacts</p>)} */}
+       
+//     </Container>
+//     )
+// };
